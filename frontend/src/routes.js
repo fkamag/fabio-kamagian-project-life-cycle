14876e8d83501879714736b3939fe8edc43f0b6e
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-08-31 20:16:06 -0300

    feat: add new routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import RegisterProject from './pages/RegisterProject';
 import StudentDataProject from './pages/StudentDataProject';
+import StudentList from './pages/StudentList';
 
 export default function Routes() {
 	return (
@@ -10,6 +12,8 @@ export default function Routes() {
 			<Switch>
 				<Route path="/" exact component={Home}></Route>
 				<Route path="/student/data/project" exact component={StudentDataProject}></Route>
+				<Route path="/project/new/" exact component={RegisterProject}></Route>
+				<Route path="/student/class" exact component={StudentList}></Route>
 			</Switch>
 		</BrowserRouter>
 	);
