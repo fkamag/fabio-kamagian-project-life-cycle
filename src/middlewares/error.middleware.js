2fcf907e1b22d0a408db8ced729de4d683c2a2ac
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-08-31 14:20:45 -0300

    refactor: remove backend old files

--- src/middlewares/error.middleware.js
+++
@@ -1,6 +0,0 @@
-const errorMiddleware = (err, _req, res, _next) => {
-    console.error("Error middleware", err);
-    res.status(500).json({ message: err.message });
-}
-
-module.exports = errorMiddleware;
