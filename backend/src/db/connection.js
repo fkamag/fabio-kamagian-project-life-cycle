9e091dba27ba4cb5c53c093e95c73676c19c1493
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-08-31 20:15:58 -0300

    feat: implements .env to connection

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "123456",
-	database: "blacklinters"
+	host: process.env.DATABASE_HOST || "database",
+	port: process.env.DATABASE_PORT || 3306,
+	user: process.env.DATABASE_USER || "root",
+	password: process.env.DATABASE_PASSWORD || "xablau",
+	database: process.env.DATABASE_NAME || "blacklinters"
 });
 
 module.exports = connection;
